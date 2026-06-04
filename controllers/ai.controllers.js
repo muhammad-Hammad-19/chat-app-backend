82825c2c081471d0877f0707fe79d1aa39624ab0
Muhammad Hammad <mh03212868@gmail.com>
2026-06-04 19:59:11 +0500

    add vercel json file for backend deployment

--- controllers/ai.controllers.js
+++ controllers/ai.controllers.js
@@ -14,7 +14,7 @@ Return only replies, each on new line.
 `;
 
   const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
+    model: "gemini-2.5-flash",
     contents: prompt,
   });
 
